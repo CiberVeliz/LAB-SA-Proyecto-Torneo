594ae96f67590f891a2b7f5199d9ed9fdafc0b94
CiberVeliz <Ciberveliz97@gmail.com>
2020-10-30 01:14:41 -0600

    registro partida

--- app/index.js
+++ app/index.js
@@ -4,6 +4,12 @@ var uuid = require('uuid');//uuid.v4()
 
 app.use(express.static(__dirname + '/public/'));
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
 // ---------------------- DATABASE ---------------------------------------
 const sqlite3 = require('sqlite3').verbose();
 
@@ -16,18 +22,37 @@ let db = new sqlite3.Database('./database/database.db', (err) => {
 });
 // -------------------------------------------------------------------------
 
+// --------------------- FUNCIONAMIENTO TORNEOS -----------------------------
+
+
+
+// ---------------------------------------------------------------------------
 
 //------------- PARTIDAS ----------------------
+
+//genera un partida
+app.get('/generarPartida', function(req, res)
+{
+  let id = uuid.v4();
+  console.log(id);
+
+  res.status(200);
+})
+
+//registra una partida
 app.put('/partidas/:id', async function (req, res) 
 {
   //ab89f0c1-31bb-48d6-84f0-00ff0514780c
     var id = req.params.id;
-    
-    if(!id || !isNaN(id))
+    var marcador = req.body.marcador;
+
+    if(!id || !isNaN(id) || !marcador)
     {
       res.status(406).send('Parámetros no válidos');
       return;
     }
+
+    let obj_marcador = JSON.parse(marcador);
 
     await db.get("SELECT COUNT(1) FROM Partida WHERE id = $id", {$id: id}, (error, row) => {
       if(row['COUNT(1)'] === 0)
@@ -37,6 +62,10 @@ app.put('/partidas/:id', async function (req, res)
       }
       else
       {
+        let query = 'UPDATE Partida SET ganador = ? WHERE id = ?';
+
+        db.run(query, [obj_marcador[0], id]);
+
         res.status(201).send('Partida registrada correctamente.');
       }
     });
