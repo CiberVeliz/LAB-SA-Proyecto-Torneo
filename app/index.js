8f788ece820f95172ba1d16e29fd34cf6d9f8bb9
CiberVeliz <Ciberveliz97@gmail.com>
2020-10-30 02:35:07 -0600

    fix json body

--- app/index.js
+++ app/index.js
@@ -2,13 +2,25 @@ const express = require('express');
 const app = express();
 var uuid = require('uuid');//uuid.v4()
 
-app.use(express.static(__dirname + '/public/'));
+var bodyParser = require('body-parser')
 
-app.use(
-  express.urlencoded({
-    extended: true
-  })
-)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(__dirname + '/public/'));
 
 // ---------------------- DATABASE ---------------------------------------
 const sqlite3 = require('sqlite3').verbose();
@@ -43,6 +55,7 @@ app.get('/generarPartida', function(req, res)
 app.put('/partidas/:id', async function (req, res) 
 {
   //ab89f0c1-31bb-48d6-84f0-00ff0514780c
+  console.log(req.body)
     var id = req.params.id;
     var marcador = req.body.marcador;
 
